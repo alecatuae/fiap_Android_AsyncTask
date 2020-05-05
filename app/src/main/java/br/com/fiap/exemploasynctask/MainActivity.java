50ea1357c676e171ce25c40e66f885658c0f7bfb
Alexandre Nascimento <alecatuae@gmail.com>
2020-05-04 23:05:12 -0300

    Android Async Task

---
+++ app/src/main/java/br/com/fiap/exemploasynctask/MainActivity.java
@@ -0,0 +1,80 @@
+package br.com.fiap.exemploasynctask;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressDialog mProgressDialog;
+    private ImageView imgDownload;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgDownload = findViewById(R.id.imgDownload);
+    }
+
+    public void downloadImagem(View view) {
+        DownloadAsyncTask downloadAsyncTask = new DownloadAsyncTask();
+        downloadAsyncTask.execute("https://i.picsum.photos/id/237/500/800.jpg");
+
+    }
+
+    private class DownloadAsyncTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            mProgressDialog = ProgressDialog.show(
+                    MainActivity.this, "Aguarde", "Baixando a Imagem...");
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+
+                InputStream inputStream;
+                Bitmap imagem;
+
+                URL endereco = new URL(strings[0]);
+                inputStream = endereco.openStream();
+
+                imagem = BitmapFactory.decodeStream(inputStream);
+
+                inputStream.close();
+
+                return imagem;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+
+            if (bitmap != null) {
+                imgDownload.setImageBitmap(bitmap);
+            }
+            mProgressDialog.dismiss();
+        }
+    }
+
+
+}
